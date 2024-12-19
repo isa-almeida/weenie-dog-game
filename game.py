7796cfa6c51ac20cc6c0fb455dad882e26d1a9ce
Isabel Almeida <almeida.isabel.cp@gmail.com>
2024-12-18 21:12:14 -0300

    finish drawing

--- game.py
+++ game.py
@@ -1,6 +1,18 @@
 import pygame, sys, random
 from pygame.math import Vector2
 
+class DOG:
+    def __init__(self):
+        self.body = [Vector2(5,10),Vector2(6,10),Vector2(7,10)]
+    
+    def draw_dog(self):
+        for block in self.body:
+            x_pos = int(block.x * cell_size)
+            y_pos = int(block.y * cell_size)
+            #create a rect
+            block_rect = pygame.Rect(x_pos, y_pos, cell_size, cell_size)
+            #draw the rec
+            pygame.draw.rect(screen,(183,111,122), block_rect)
 class HOT_DOG:
     def __init__(self):
         # create an x and y position
@@ -22,7 +34,8 @@ screen_size = (cell_number * cell_size)
 screen = pygame.display.set_mode((screen_size,screen_size))
 clock = pygame.time.Clock() #set maximum frames per second
 
-fruit = HOT_DOG()
+hotdog = HOT_DOG()
+dog = DOG()
 
 #loop for running the game
 while True:
@@ -33,6 +46,7 @@ while True:
             sys.exit() #ends any kind of code thats running on
     
     screen.fill((175,215,70))          
-    fruit.draw_hotdog()
+    hotdog.draw_hotdog()
+    dog.draw_dog()
     pygame.display.update()
     clock.tick(60) #maximum 60 frames per second (adds consistency to the game)
